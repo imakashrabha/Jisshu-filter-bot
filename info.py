552eaa7d1ac0cce384e7c49cabfbc50689322ce0
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-02-01 00:15:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8006068020:AAHMC8x6J1gs9TSyt-z2HQYoX_ZzXVu
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6165669080').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Wtf_u_want") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002369020908'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/JisshuMovieZone')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/OxmnfufB85hYjc9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002251640921').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
