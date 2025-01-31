6d033a0b334ee91b5c9dbf286920f8fed03a68ee
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-02-01 00:25:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002193477014')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/OxmnfufB85hYjc9') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
