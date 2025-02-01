366d669b134201bc58b7eafc86ef1ad360a2364d
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-02-01 12:20:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -315,10 +315,11 @@ async def start(client:Client, message):
             else:
                 howtodownload = settings.get('tutorial_2', TUTORIAL_2) if is_second_shortener else settings.get('tutorial', TUTORIAL)
             buttons = [[
-                InlineKeyboardButton(text="✅ ᴠᴇʀɪғʏ ✅", url=verify),
-                InlineKeyboardButton(text="ʜᴏᴡ ᴛᴏ ᴠᴇʀɪғʏ❓", url=howtodownload)
+                InlineKeyboardButton(text="• ᴏᴘᴇɴ ʟɪɴᴋ", url=verify),
                 ],[
-                InlineKeyboardButton(text="😁 ʙᴜʏ sᴜʙsᴄʀɪᴘᴛɪᴏɴ - ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴠᴇʀɪғʏ 😁", callback_data='seeplans'),
+                InlineKeyboardButton(text="ᴛᴜᴛᴏʀɪᴀʟ •", url=howtodownload)
+                ],[
+                InlineKeyboardButton(text="ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ", callback_data='seeplans'),
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             if await db.user_verified(user_id): 
